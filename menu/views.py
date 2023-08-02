a9e59e265c205b5e12b423e005c4f4e46af4c7e9
hashtag-squirrel <alita2000@web.de>
2023-08-02 19:33:52 +0200

    Add get_context_data method to MenuView class to show second model in view

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views import generic, View
 from menu.models import MenuCategory, MenuItem
@@ -6,5 +7,10 @@ from menu.models import MenuCategory, MenuItem
 class MenuView(generic.ListView):
 
     model = MenuCategory
-    queryset = MenuCategory.objects.all()
+    categories = MenuCategory.objects.all()
     template_name = 'menu.html'
+
+    def get_context_data(self, **kwargs: Any):
+        ctx = super(MenuView, self).get_context_data(**kwargs)
+        ctx['menu_items'] = MenuItem.objects.all()
+        return ctx
