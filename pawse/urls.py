583411e89f081313c6615ab61280addc621202d5
hashtag-squirrel <alita2000@web.de>
2023-08-05 16:42:28 +0200

    Add cat content to cats template

--- pawse/urls.py
+++ pawse/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls'), name='pages-urls'),
     path('menu/', include('menu.urls'), name='menu-urls'),
     path('cats/', include('cats.urls'), name='cats-urls'),
-]
+] 
