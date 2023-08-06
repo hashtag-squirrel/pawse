08f16fc516d8aaa58ba46ae4e389d81e70f32a29
hashtag-squirrel <alita2000@web.de>
2023-08-06 11:35:13 +0200

    Add conditional statement to DEBUG in settings

--- pawse/settings.py
+++ pawse/settings.py
@@ -29,7 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.path.isfile('env.py'):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['pawse-4504aa9c3705.herokuapp.com', '127.0.0.1']
 
