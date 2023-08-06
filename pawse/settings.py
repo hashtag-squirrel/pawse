71395a0a51d6760a4fca7ee6b9b1de68523f8967
hashtag-squirrel <alita2000@web.de>
2023-08-06 13:40:17 +0200

    Change DEBUG to False

--- pawse/settings.py
+++ pawse/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT'
+DEBUG = False
 
 ALLOWED_HOSTS = ['pawse-4504aa9c3705.herokuapp.com', '127.0.0.1']
 
